fd711d8194845ce660a7a33f0b28c1f41b345a14
William Cahill-Manley <william@cahillmanley.com>
2013-09-04 12:00:27 -0700

    Initial Commit

---
+++ src/Kindari/BladeAlternator/BladeAlternator.php
@@ -0,0 +1,34 @@
+<?php namespace Kindari\BladeAlternator;
+
+class BladeAlternator { 
+
+        public $counts = array();
+
+        public function increment($key) {
+
+                if ( ! array_key_exists($key, $this->counts) ) $this->reset($key);
+                $this->counts[$key]++;
+                return $this->counts[$key];
+        }
+
+        public function reset($key) {
+                $this->counts[$key] = 0;
+                return $this;
+        }
+
+        public function choose() {
+
+        	$items = func_get_args();
+            $index = $this->increment($key = $this->getKey() );
+            if ($index == count($items)) $this->reset($key);
+            return $items[$index - 1];
+        }
+
+        public function getKey() {
+
+        	$trace = debug_backtrace()[1];
+        	return "{$trace['file']}@{$trace['line']}";
+
+        }
+
+}
---
+++ src/Kindari/BladeAlternator/BladeAlternatorServiceProvider.php
@@ -0,0 +1,57 @@
+<?php namespace Kindari\BladeAlternator;
+
+use Illuminate\Support\ServiceProvider;
+
+class BladeAlternatorServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerAlternator();
+
+		$this->registerBladeExtension();
+	}
+
+	public function registerAlternator() {
+
+		$this->app['blade.alternator'] = $this->app->share(function($app) {
+			return new BladeAlternator;
+		});
+
+	}
+
+	public function registerBladeExtension() {
+
+		$compiler = $this->app['view.engine.resolver']->resolve('blade')->getCompiler();
+		$compiler->extend(function($value, $blade){
+
+			$pattern = $blade->createMatcher('alternator');
+			return preg_replace($pattern, "$1<?php echo app('blade.alternator')->choose$2; ?>", $value);
+		});
+
+	}
+
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('blade.alternator');
+	}
+
+}
